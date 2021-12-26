fd571bfad621ae535b767990ec138c53383ebf00
ppmasa8 <kiyokiyopip56@gmail.com>
2021-12-27 01:19:48 +0900

    Error handling

--- lib/calc.dart
+++ lib/calc.dart
@@ -3,54 +3,69 @@ const operand = ['+', '-', '×', '÷', 'mod'];
 class Calculator {
   static var _list_number  = [];
   static var _list_operand = [];
+  static var _list         = [];
 
   static var _buffer = '';
 
   static void GetKey(String letter) {
+    //TODO Fix backspace action.
     if (operand.contains(letter)) {
       _list_operand.add(letter);
       _list_number.add(double.parse(_buffer));
       _buffer = '';
+      _list.add(letter);
     } else if (letter == 'C') {
       _list_number.clear();
       _list_operand.clear();
       _buffer = '';
+      _list.clear();
     } else if (letter == '=') {
       return null;
     } else if (letter == '◀') {
-      if (_buffer == '')
+      var last = _list.removeLast();
+      if (operand.contains(last)) {
         _list_operand.removeLast();
-      else
-        _buffer = _buffer.substring(0, _buffer.length - 1);
+      } else {
+        _list_operand.removeLast();
+      }
     } else
       _buffer += letter;
+      _list.add(letter);
   }
 
   static String Execute() {
     var _result = 0.0;
-    _list_number.add(double.parse(_buffer));
+    try {
+      _list_number.add(double.parse(_buffer));
 
-    if (_list_number.length == 0)
-      return '0';
+      if (_list_number.length == 0)
+        return '0';
 
-    _result += _list_number[0];
-    _list_number.removeAt(0);
-    for (int i = 0; i < _list_operand.length; i++) {
-      if (_list_operand[i] == '+')
-        _result += _list_number[0];
-      else if (_list_operand[i] == '-')
-        _result -= _list_number[0];
-      else if (_list_operand[i] == '×')
-        _result *= _list_number[0];
-      else if (_list_operand[i] == '÷')
-        _result /= _list_number[0];
-      else if (_list_operand[i] == 'mod')
-        _result %= _list_number[0];
-      else
-        return 'e';
-
+      _result += _list_number[0];
       _list_number.removeAt(0);
+      for (int i = 0; i < _list_operand.length; i++) {
+        if (_list_operand[i] == '+')
+          _result += _list_number[0];
+        else if (_list_operand[i] == '-')
+          _result -= _list_number[0];
+        else if (_list_operand[i] == '×')
+          _result *= _list_number[0];
+        else if (_list_operand[i] == '÷')
+          _result /= _list_number[0];
+        else if (_list_operand[i] == 'mod')
+          _result %= _list_number[0];
+        else
+          return 'e';
+
+        _list_number.removeAt(0);
+      }
+    } catch(e) {
+      _list_number.clear();
+      _list_operand.clear();
+      _buffer = '';
+      return '';
     }
+
 
     _list_number.clear();
     _list_operand.clear();
